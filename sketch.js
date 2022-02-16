11a7eacf2e20176e880224a6dd5e2389903e15c8
luiza2008 <97768709+luiza2008@users.noreply.github.com>
2022-02-16 20:10:00 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+
+
+for (var numeros = 0; numeros < marks.length; numeros++){
+
+  if (marks[numeros] >= 45 )
+
+ { console.log (marks[numeros])
+
+ }
+}
+
+}
+
+function draw() 
+{
+  background(30);
+
+}
+
+
+
+
+ 
+
